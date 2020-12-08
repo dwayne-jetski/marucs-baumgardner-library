a9edb96a7c92c2c88b0578bb152b760926c2497d
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-12-07 19:41:36 -0600

    added book display and navigation to app.js, createed BookViewer/bookViewer.css and BookViewer/bookViewer.jsx in Componennts folder

--- src/componenets/app.jsx
+++ src/componenets/app.jsx
@@ -12,12 +12,44 @@ class App extends Component{
             this.state = {
                 bookNumber: 0,
             }
-        
+    }
+
+    goToNextBook(){
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+    goToPreviousBook(){
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--
+        if(tempBookNumber < 0){
+            tempBookNumber = this.books.length -1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
     }
     render(){
         return(
             <div className="container-fluid">
                 <TitleBar/>
+                <div className="row row-spacer">
+                    <div className="col">
+                    <button onClick={()=> this.goToPreviousBook()}>Next Book</button>
+                    </div>
+                    <div className="col-md-4">
+                        <h1>{this.books[this.state.bookNumber].title}</h1>
+                        <h4>{this.books[this.state.bookNumber].author}</h4>
+                    </div>
+                    <div className="col">
+                        <button onClick={()=> this.goToNextBook()}>Next Book</button>
+                    </div>
+                </div>
             </div>
         )
     }
