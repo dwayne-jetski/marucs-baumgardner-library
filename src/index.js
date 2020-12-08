8df622b6281d2b844d47c49c8a363a792bed3c6f
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-12-07 18:55:15 -0600

    created index.js, components folder, and app.jsx

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+
+const jsxElement = <h1>Our React App</h1>
+console.log(jsxElement);
+
+ReactDom.render(jsxElement, document.getElementById("root"));
+
