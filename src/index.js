e2888169c7d4bee14abb0ea4f01d8e8f94833d9b
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-12-07 18:59:00 -0600

    imported App from app.jsx into index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
+import App from "./componenets/app"
 
 
-const jsxElement = <h1>Our React App</h1>
-console.log(jsxElement);
 
-ReactDom.render(jsxElement, document.getElementById("root"));
+ReactDom.render(<App/>, document.getElementById("root"));
 
