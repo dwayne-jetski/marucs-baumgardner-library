c4406e7e3c4786109a3bd113a8c379c93c503547
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-12-09 17:50:26 -0600

    added bookCreator and footer files

--- src/componenets/app.jsx
+++ src/componenets/app.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import TitleBar from "./TitleBar/titleBar";
 import BookViewer from "./BookViewer/bookViewer"
+import BookCreator from './BookCreator/bookCreator'
+import Footer from './Footer/footer'
 
 class App extends Component{
     constructor(props){
@@ -13,6 +15,13 @@ class App extends Component{
             this.state = {
                 bookNumber: 0,
             }
+    }
+
+    addNewBook(book){
+        this.books.push(book);
+        this.setState({
+            bookNumber: this.books.length - 1
+        });
     }
 
     goToNextBook(){
@@ -40,6 +49,8 @@ class App extends Component{
             <div className="container-fluid">
                 <TitleBar/>
                 <BookViewer book={this.books[this.state.bookNumber]} nextBook={()=> this.goToNextBook()} previousBook={()=>this.goToPreviousBook()}/>
+                <BookCreator addNewBook={this.addNewBook.bind(this)}/>
+                <Footer />
             </div>
         )
     }
